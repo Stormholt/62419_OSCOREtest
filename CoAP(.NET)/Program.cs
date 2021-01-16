87d0f895999ee0459d695a389f159925a328e81b
Ajs <arist@adm.dtu.dk>
2021-01-16 11:49:47 +0100

    hdh

--- CoAP(.NET)/Program.cs
+++ CoAP(.NET)/Program.cs
@@ -1,104 +1,26 @@
 ﻿using Com.AugustCellars.CoAP;
 using Com.AugustCellars.CoAP.Server;
 using Com.AugustCellars.CoAP.Server.Resources;
-using Com.AugustCellars.CoAP.Util;
 using Com.AugustCellars.CoAP.OSCOAP;
-using System;
-using System.IO;
-using PeterO.Cbor;
-using Com.AugustCellars.COSE;
-
 using System.Text;
-
 using Com.AugustCellars.CoAP.Net;
+
+//Based on test of OSCOAP from :https://github.com/jimsch/CoAP-CSharp
 
 namespace CoAP_.NET_
 {
     class Program
     {
-        private static readonly CBORObject _UsageKey = CBORObject.FromObject("usage");
+
         static void Main(string[] args)
         {
-            /*  Console.WriteLine("Hello World!");
-              string filename = null;
-              SecurityContext ctx = LoadContext(filename);
-              // create a new server
-              CoapServer server = new CoapServer();
-              server.Add(new HelloWorldResource());
-              server.SecurityContexts.Add(ctx);
-              server.Start();
-            //  Console.WriteLine(server.SecurityContexts.ToString());
 
-              Request request = new Request(Method.GET);
-              //  SecurityContextSet set = LoadContextSet(filename);
-
-              request.OscoreContext = ctx;
-              request.URI = new Uri("coap://192.168.0.13/hello-world");
-              request.Send();
-
-              // wait for one response
-              Response response = request.WaitForResponse();
-
-              Console.WriteLine(Utils.ToString(response));*/
 
             Oscoap os = new Oscoap();
 
             os.SetupServer();
             os.Ocoap_Get();
             os.ShutdownServer();
-
-        }
-
-        private static SecurityContextSet LoadContextSet(string fileName)
-        {
-            if (fileName == null) fileName = "ServerKeys.cbor";
-            KeySet keys = new KeySet();
-            SecurityContextSet newSet = new SecurityContextSet();
-
-            FileStream fs = new FileStream(fileName, FileMode.Open);
-            using (BinaryReader reader = new BinaryReader(fs))
-            {
-                byte[] data = reader.ReadBytes((int)fs.Length);
-                CBORObject obj = CBORObject.DecodeFromBytes(data);
-                for (int i = 0; i < obj.Count; i++)
-                {
-                    OneKey key = new OneKey(obj[i]);
-                    SecurityContext ctx = SecurityContext.DeriveContext(
-                               key[CoseKeyParameterKeys.Octet_k].GetByteString(),
-                               null,
-                               key[CBORObject.FromObject("RecipID")].GetByteString(),
-                               key[CBORObject.FromObject("SenderID")].GetByteString(), null,
-                               key[CoseKeyKeys.Algorithm]);
-                    newSet.Add(ctx);
-                }
-            }
-            return newSet;
-        }
-        private static SecurityContext LoadContext(string fileName)
-        {
-            if (fileName == null) fileName = "ServerKeys.cbor";
-            KeySet keys = new KeySet();
-            SecurityContext newSet = new SecurityContext();
-
-            FileStream fs = new FileStream(fileName, FileMode.Open);
-            using (BinaryReader reader = new BinaryReader(fs))
-            {
-                byte[] data = reader.ReadBytes((int)fs.Length);
-                CBORObject obj = CBORObject.DecodeFromBytes(data);
-              //  for (int i = 0; i < obj.Count; i++)
-                //{
-                    OneKey key = new OneKey(obj[0]);
-                    SecurityContext ctx = SecurityContext.DeriveContext(
-                               key[CoseKeyParameterKeys.Octet_k].GetByteString(),
-                               null,
-                               key[CBORObject.FromObject("RecipID")].GetByteString(),
-                               key[CBORObject.FromObject("SenderID")].GetByteString(), null,
-                               key[CoseKeyKeys.Algorithm]);
-                    newSet = ctx;
-               // }
-            }
-            return newSet;
-
 
         }
 
@@ -117,7 +39,6 @@ namespace CoAP_.NET_
                 CreateServer();
             }
 
-
             public void ShutdownServer()
             {
                 _server.Dispose();
@@ -130,10 +51,6 @@ namespace CoAP_.NET_
                     OscoreContext = SecurityContext.DeriveContext(secret, null, clientId, serverId)
                 };
                 Response r = client.Get();
-              //  Console.WriteLine();
-               // Console.WriteLine(Utils.ToString(r));
-                // Assert.IsNotNull(r);
-                // Assert.AreEqual("/abc", r.PayloadString);
             }
 
             private void CreateServer()
@@ -141,13 +58,8 @@ namespace CoAP_.NET_
                 CoAPEndPoint endpoint = new CoAPEndPoint(0);
                 _server = new CoapServer();
 
-                //           _resource = new StorageResource(TARGET, CONTENT_1);
-              //             _server.Add(_resource);
-
                 Resource r2 = new HelloWorldResource();
                 _server.Add(r2);
-
-                //r2.Add(new EchoLocation("def"));
 
                 _server.AddEndPoint(endpoint);
                 _server.Start();
@@ -158,7 +70,6 @@ namespace CoAP_.NET_
             }
         }
 
-  
             class HelloWorldResource : Resource
         {
             public HelloWorldResource()
